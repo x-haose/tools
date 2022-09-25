cd9fe1bab6760673cfac0c242e8402991c91940b
昊色居士 <xhrtxh@gmail.com>
2022-09-25 23:17:43 +0800

    fix: 修改安装pgsql脚本

--- install_pgsql.sh
+++ install_pgsql.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt buster-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt buster-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt update
 sudo apt install postgresql-13 -y
 sudo passwd -d postgres
 su - -c "psql -c $'ALTER USER postgres WITH PASSWORD \'123456\';'" postgres
 sudo sed -i 's@127.0.0.1/32@0.0.0.0/0@g' /etc/postgresql/13/main/pg_hba.conf
+sudo sed -i '60s/^.//' /etc/postgresql/13/main/postgresql.conf
+sudo sed -i '60s/localhost/*/' /etc/postgresql/13/main/postgresql.conf
 
 sudo pg_ctlcluster 13 main start
 sudo /etc/init.d/postgresql restart postgresql
 
+echo "\n\nPGSQL13 安装完毕!!! \n默认用户名：postgresql \n默认密码：123456 \n配置文件位置：/etc/postgresql/13/main/postgresql.conf"
